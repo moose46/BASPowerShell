25b9dadbb413c42c611e3f1e93d3997fd5a1ea7f
me <me@robertcurtiss.com>
2020-03-17 08:12:52 -0400

    Monday Night Checkin

--- CreateAllZipFiles.ps1
+++ CreateAllZipFiles.ps1
@@ -3,31 +3,29 @@ Set-Variable -Name EXPORT_PATH -Value 'C:\Users\me\Source\Repos\BabbleFishV4\Bab
 Set-Variable -Name ERROR_FILENAME -Value 'C:\Users\me\Source\Repos\BabbleFishV4\BabbleFishV3\crm\data_to_crm\error.log'
 $PROCESS_ALL_FILES = '.\ProcessAllFiles.ps1'
 
-cd $POWERSHELL_PATH
+Set-Location $POWERSHELL_PATH
 
-if (Test-Path $ERROR_FILENAME) { del $ERROR_FILENAME }
+if (Test-Path $ERROR_FILENAME) { Remove-Item $ERROR_FILENAME }
 
 
-for($year=2015; $year -lt 2021; $year++)
-{
-    for($quarter= 1; $quarter -lt 5;$quarter++)
-    {
+for ($year = 2015; $year -lt 2021; $year++) {
+    for ($quarter = 1; $quarter -lt 5; $quarter++) {
 
         Write-Host "Processing...." $year $quarter -foreground Green
         Powershell -Command "C:\Users\me\Source\Repos\BabbleFishV4\BabbleFishV3\powershell\CreateCSV_ATTACHMENT_params.ps1 $year $quarter"
     }
 }
 
-cd $EXPORT_PATH
-if (Test-Path $PROCESS_ALL_FILES) {Remove-Item $PROCESS_ALL_FILES}
+Set-Location $EXPORT_PATH
+if (Test-Path $PROCESS_ALL_FILES) { Remove-Item $PROCESS_ALL_FILES }
 
 "Set-Variable -Name ATTACHMENTS -Value 'E:\NETSUITE ATTACHMENTS\'" > $PROCESS_ALL_FILES
 
 "cd 'e:\netsuite attachments'" >> $PROCESS_ALL_FILES
 "if (Test-Path 'e:\netsuite attachments\myerror.log') {Remove-Item myerror.log}" >> $PROCESS_ALL_FILES
 Get-ChildItem *.ps1 | foreach-Object {
-    if($_.Name -notcontains "ProcessAllFiles.ps1") {
+    if ($_.Name -notcontains "ProcessAllFiles.ps1") {
         #Write-Host $_.Name -ForegroundColor Green
-        'PowerShell -Command .\'+$_.Name+' 2>>myerror.log' >> $PROCESS_ALL_FILES
+        'PowerShell -Command .\' + $_.Name + ' 2>>myerror.log' >> $PROCESS_ALL_FILES
     }
 }
