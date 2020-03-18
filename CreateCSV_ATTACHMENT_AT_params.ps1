84772c57dd2d5462b99e3cfe6be936c718b42f4a
me <me@robertcurtiss.com>
2020-03-18 06:26:59 -0400

    Tuesday Morning Checkin

--- CreateCSV_ATTACHMENT_AT_params.ps1
+++ CreateCSV_ATTACHMENT_AT_params.ps1
@@ -9,3 +9,5 @@ Param(
 )
 
 sqlcmd -d babblefish -o error.log -E  -i "C:\Users\me\Source\Repos\BabbleFishV4\BabbleFishV3\sql_querys\Attachment_AT_params.sql" -v p_year=2016 p_quarter=2 --% -S daffY-duck
+
+
